316bfc380b116ed496b04c124c09c45e9505ba79
Ryan Lider <rslider@shaw.ca>
2016-06-29 23:53:43 -0700

    Added functioning New, Create, Show, Update and Edit methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :articles
+
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
 
---
+++ db/migrate/20160630061929_add_created_at_to_articles.rb
@@ -0,0 +1,5 @@
+class AddCreatedAtToArticles < ActiveRecord::Migration
+  def change
+  
+  end
+end

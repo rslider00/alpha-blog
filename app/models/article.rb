d966e8267c2311e7accac3cbedb57a44d2588665
Ryan Lider <rslider@shaw.ca>
2016-08-15 19:05:42 -0700

    Added show page with images and fixed email problem with Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
-	before_save {self.email = email.downcase}
+
 	validates :title, presence: true, length: {minimum: 3, maximum: 50}
 	validates :description, presence: true, length: {minimum: 10, maximum: 300}
 	validates :user_id, presence: true
